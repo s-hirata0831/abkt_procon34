417b26cff132792d0f31f53b2943557171548069
s-hirata0831 <e9248@g.maizuru-ct.ac.jp>
2023-09-15 15:37:33 +0900

    ヘッダーの形づくり(もう少し手を加えます)

--- abkt-app/src/App.jsx
+++ abkt-app/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Button } from "@mui/material";
+import Header from './components/modules/Header';
 
 export const App = () => {
   return (
     <>
+    <Header />
     <p>Hello Abkt!</p>
     <Button>日本人</Button>
     <Button variant="contained">contained</Button>
