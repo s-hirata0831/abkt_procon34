9b4d7e1f4af943f29f741b3d56a48ec8710d09f1
HIRATA Soma <e9248@g.maizuru-ct.ac.jp>
2023-10-09 15:30:30 +0900

    feat: FireStoreに部屋立てデータを送る

--- abkt-app/src/components/pages/CreateRoom.jsx
+++ abkt-app/src/components/pages/CreateRoom.jsx
@@ -16,8 +16,21 @@ export const CreateRoom = () => {
     const [exampleIdea, setExampleIdea] = useState("");
     const [limitSecondPhase, setLimitSecondPhase] = useState("");
     const [peopleAmount, setPeopleAmount] = useState("");
-    const inputRef = useRef(null);
-    const [inputError, setInputError] = useState(false);
+
+    //入力内容が規則に沿っているかの評価関数
+    const [teamNameError, setTeamNameError] = useState(false);
+    const [ideaThemeError, setIdeaThemeError] = useState(false);
+    const [exampleIdeaError, setExampleIdeaError] = useState(false);
+    const [peopleAmountError, setPeopleAmountError] = useState(false);
+    const [limitSecondPhaseError, setLimitSecondPhaseError] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+
+    const teamNameInputRef = useRef(null);
+    const ideaThemeInputRef = useRef(null);
+    const exampleIdeaInputRef = useRef(null);
+    const peopleAmountInputRef = useRef(null);
+    const limitSecondPhaseInputRef = useRef(null);
+    const passwordInputRef = useRef(null);
 
     const createRoom = async () => {
         //ランダムなIDを持つ部屋ドキュメント(Collection)を生成
@@ -34,13 +47,13 @@ export const CreateRoom = () => {
         });
     };
 
-    const handleChange = () => {
+    const handleChange = (inputRef, setError) => {
         if (inputRef.current) {
             const ref = inputRef.current;
             if (!ref.validity.valid) {
-                setInputError(true);
+                setError(true);
             } else {
-                setInputError(false);
+                setError(false);
             }
         }
     };
@@ -58,28 +71,104 @@ export const CreateRoom = () => {
                         <p className={CRModule.body_text}>全て入力必須項目です。</p>
                         <TextField
                             fullWidth
+                            required
                             margin="normal"
-                            error={inputError}
-                            inputProps={{ maxLength: 10, minLength: 2 }}
-                            inputRef={inputRef}
+                            error={teamNameError}
+                            inputProps={{ maxLength: 10, minLength: 1 }}
+                            inputRef={teamNameInputRef}
                             defaultValue={""}
-                            label="チーム名"
+                            label="チーム名 [10文字以内]"
                             variant="standard"
-                            helperText={inputRef?.current?.validationMessage}
-                            onChange={(e) => setTeamName(e.target.value)}
+                            helperText={teamNameInputRef?.current?.validationMessage}
+                            onChange={(e) => {
+                                setTeamName(e.target.value);
+                                handleChange(teamNameInputRef, setTeamNameError);
+                            }}
                         />
                         <TextField
                             fullWidth
+                            required
                             margin="normal"
-                            error={inputError}
-                            inputProps={{ maxLength: 10, minLength: 2 }}
-                            inputRef={inputRef}
+                            error={ideaThemeError}
+                            inputProps={{ maxLength: 25, minLength: 1 }}
+                            inputRef={ideaThemeInputRef}
                             defaultValue={""}
-                            label="アイデア(呪文)を出すテーマ"
+                            label="アイデア(呪文)を出すテーマ [25文字以内]"
                             variant="standard"
-                            helperText={inputRef?.current?.validationMessage}
-                            onChange={(e) => setTeamName(e.target.value)}
+                            helperText={ideaThemeInputRef?.current?.validationMessage}
+                            onChange={(e) => {
+                                setIdeaTheme(e.target.value);
+                                handleChange(ideaThemeInputRef, setIdeaThemeError);
+                            }}
                         />
+                        <TextField
+                            fullWidth
+                            required
+                            margin="normal"
+                            error={exampleIdeaError}
+                            inputProps={{ maxLength: 25, minLength: 1 }}
+                            inputRef={exampleIdeaInputRef}
+                            defaultValue={""}
+                            label="アイデア(呪文)の例 [25文字以内]"
+                            variant="standard"
+                            helperText={exampleIdeaInputRef?.current?.validationMessage}
+                            onChange={(e) => {
+                                setExampleIdea(e.target.value);
+                                handleChange(exampleIdeaInputRef, setExampleIdeaError);
+                            }}
+                        />
+                        <TextField
+                            fullWidth
+                            required
+                            margin="normal"
+                            error={peopleAmountError}
+                            type="number"
+                            inputProps={{ min: 2, max: 50 }}
+                            inputRef={peopleAmountInputRef}
+                            defaultValue={""}
+                            label="参加人数 [2人以上]"
+                            variant="standard"
+                            helperText={peopleAmountInputRef?.current?.validationMessage}
+                            onChange={(e) => {
+                                setPeopleAmount(e.target.value);
+                                handleChange(peopleAmountInputRef, setPeopleAmountError);
+                            }}
+                        />
+                        <TextField
+                            fullWidth
+                            required
+                            margin="normal"
+                            error={limitSecondPhaseError}
+                            type="time"
+                            inputProps={{ min: "00:30" }}
+                            inputRef={limitSecondPhaseInputRef}
+                            defaultValue="00:30"
+                            label="第2フェーズの制限時間 [30秒以上]"
+                            variant="standard"
+                            helperText={limitSecondPhaseInputRef?.current?.validationMessage}
+                            onChange={(e) => {
+                                setLimitSecondPhase(e.target.value);
+                                handleChange(limitSecondPhaseInputRef, setLimitSecondPhaseError);
+                            }}
+                        />
+                        <TextField
+                            fullWidth
+                            required
+                            margin="normal"
+                            error={passwordError}
+                            inputProps={{ maxLength: 25, minLength: 2, pattern:"^[0-9A-Za-z]+$" }}
+                            inputRef={passwordInputRef}
+                            type="password"
+                            defaultValue={""}
+                            label="パスワード [半角英数字25文字以内]"
+                            variant="standard"
+                            helperText={passwordInputRef?.current?.validationMessage}
+                            onChange={(e) => {
+                                setPassword(e.target.value);
+                                handleChange(passwordInputRef, setPasswordError);
+                            }}
+                        />
+                        <Button variant="outlined" style={{color:"#7882b0"}} onClick={createRoom}>作成</Button>
                     </Box>
                 </div>
             </StyledEngineProvider>
